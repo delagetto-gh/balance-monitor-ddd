7887b0c10a0a5b367feaf19f50002d900d558fa3
delagetto <adesule1@gmail.com>
2017-02-03 14:11:15 +0000

    Created DepositAmountCommand. Renamed WithdrawMoneyCommand.cs => WithdrawAmountCommand.cs

---
+++ BalanceMonitor.Accounting.Domain.Commands/DepositAmountCommand.cs
@@ -0,0 +1,18 @@
+﻿using BalanceMonitor.Accounting.Domain.Common;
+using System;
+
+namespace BalanceMonitor.Accounting.Domain.Commands
+{
+  public class DepositAmountCommand : WindowsIdentityCommand
+  {
+    public Money Amount { get; set; }
+
+    public Guid AccountId { get; set; }
+
+    public DepositAmountCommand(Guid accountId, Money amount)
+    {
+      this.AccountId = accountId;
+      this.Amount = amount;
+    }
+  }
+}
--- BalanceMonitor.Accounting.Domain.Commands/WithdrawAmountCommand.cs
+++ BalanceMonitor.Accounting.Domain.Commands/WithdrawAmountCommand.cs
@@ -3,13 +3,13 @@ using System;
 
 namespace BalanceMonitor.Accounting.Domain.Commands
 {
-  public class WithdrawMoneyCommand : WindowsIdentityCommand
+  public class WithdrawAmountCommand : WindowsIdentityCommand
   {
     public Money Amount { get; set; }
 
     public Guid AccountId { get; set; }
 
-    public WithdrawMoneyCommand(Guid accountId, Money amount)
+    public WithdrawAmountCommand(Guid accountId, Money amount)
     {
       this.AccountId = accountId;
       this.Amount = amount;
